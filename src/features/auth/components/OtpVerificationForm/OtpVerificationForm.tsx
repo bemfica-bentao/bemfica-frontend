ccb3e72bc25baeef8b22a2ac5f0c7259fec40b3f
VictorDumer <victorgabrieldummersouza@gmail.com>
2026-05-18 00:12:51 -0300

    feat(otp): add useOtpInput hook and update form layout

--- src/features/auth/components/OtpVerificationForm/OtpVerificationForm.tsx
+++ src/features/auth/components/OtpVerificationForm/OtpVerificationForm.tsx
@@ -1,19 +1,27 @@
+
 import Background from '@/assets/images/BackgroundOTP.png'
 import Star from '@/assets/images/Star.png'
 import LineStar from '@/assets/images/LineStar.png'
 import './OtpVerificationForm.scss'
+import { useOtpInput } from '@/hooks/useOtpInput'
 
 export default function OtpVerificationForm (){
+    const { setRef, handleChange, handleKeyDown, handlePaste } = useOtpInput();
+
     return(
         <div className="MainLayout">
             <div className="LeftContainer" style={{backgroundImage: `url(${Background})`}}>
                 <div className="LeftTextContainer">
-                    <div>
+                    <div className='FirstTextContainer'>
                         <h1 className='FirstText'>
                             Verifique <br/>
                             seu <span className="ColorPurple1">e-mai</span> <br/>
                             agora<span className="ColorPurple5">.</span>
                         </h1>
+                        <div className='LineContainer'>
+                            <div className='Line' />
+                            <img src={Star} alt="Estrela" className='Star' />
+                        </div>
                     </div>
                     <p className="SecondText ColorPurple5"> 
                         Enviamos um código de 6<br/>
@@ -24,10 +32,7 @@ export default function OtpVerificationForm (){
 
                 <img src={LineStar} alt="Linha com estrelas" className='LineStar' />
 
-                <div className='LineContainer'>
-                    <div className='Line' />
-                    <img src={Star} alt="Estrela" className='Star' />
-                </div>
+                
                 
             </div>
 
@@ -42,32 +47,48 @@ export default function OtpVerificationForm (){
                     <div className="StepThird" />
                 </div>
                 <main className="MainContainer">
-                    <h2 className='MainText'>Código de verificação </h2>
-                    <p className='SubText'>Digite o código enviado <span className="ColorPurple1">para o seu e-mail =]</span></p>
-                        
-                    {/* Da para componentizar isso aqui */}
-                    <div className="EmailContainer">
-                        <p>icon</p>
-                        <p>jo****@etec.sp.gov.br</p>
+                    <div className='MainTitles'>
+                        <h2 className='MainText'>Código de verificação </h2>
+                        <p className='SubText'>Digite o código enviado <span className="ColorPurple1">para o seu e-mail =]</span></p>
                     </div>
+                    
 
-                    <form className="FormContainer">
+                            
+                        {/* Da para componentizar isso aqui */}
+                        <div className="EmailContainer">
+                            <i className="fa-regular fa-envelope"></i>
+                            <p>jo***@etec.sp.gov.br</p>
+                        </div>
+                    
+
+                    <form className="FormContainer" action='/verify'>
                         <div className="OtpFormInput">
-                            <input type="text" className="OtpInput" required/>
-                            <input type="text" className="OtpInput" required/>
-                            <input type="text" className="OtpInput" required/>
-                            <input type="text" className="OtpInput" required/>
-                            <input type="text" className="OtpInput" required/>
+                             {Array.from({ length: 6 }, (_, index) => (
+                            <input
+                            key={index}
+                            type="text"
+                            inputMode="numeric"
+                            className="OtpInput"
+                            maxLength={1}
+                            ref={element => setRef(element, index)}
+                            onChange={event => handleChange(event, index)}
+                            onKeyDown={event => handleKeyDown(event, index)}
+                            onPaste={handlePaste}
+                            required
+                            />
+                        ))}
                         </div>
 
                         <div className='InfoContainer'>
-                            <p>icone</p>
+                            <i className="fa-solid fa-circle-info"></i>
                             <p className='TextAdvice'>Apenas números. Cole o código diretamente</p>
                         </div>
-                        
-                        <button type="submit" className='SubmitButton'> Verificar código => </button>
+                        <div className='ButtonsContainer'>
 
-                        <button type="button"> reenviar código</button>
+                        <button type="submit" className='SubmitButton'> Verificar código </button>
+
+                        <button type="button" className='ResendButton'> <span className='ResendTitle'> reenviar código </span> </button>
+                        </div>
                     </form>
 
                 </main>
