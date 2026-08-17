5abf415f6a2e679680ebed5767a85871598c298b
Victor Dumer <vicdumer.dev@gmail.com>
2026-08-17 14:11:20 -0300

    [FIX] GithubPages - Adaptando rotas para o github pages

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename='/'>
+  <BrowserRouter basename={import.meta.env.BASE_URL}>
     <StrictMode>
       <App />
     </StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/bemfica-frontend/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
